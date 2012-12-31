0073da021990376ec2e4b1047bdf22727f427fcf
Kyril Revels <krevels@lotame.com>
2012-12-31 14:55:09 -0500

    restructuring

--- app.js
+++ app.js
@@ -1,56 +1,66 @@
-function ContactsView() {
-	var self = this;
+$(document).ready(function(){
+	var cm = new ContactManager();
+	cm.init();
+});
 
-	var contacts = ko.observableArray([
-		{ name: "Contact 1", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" },
-		{ name: "Contact 2", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" },
-		{ name: "Contact 3", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "friend" },
-		{ name: "Contact 4", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "colleague" },
-		{ name: "Contact 5", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" },
-		{ name: "Contact 6", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "colleague" },
-		{ name: "Contact 7", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "friend" },
-		{ name: "Contact 8", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" }
-	]);
+function ContactManager(){
+	function ContactsView() {
+		var self = this;
 
-	self.selected_type = ko.observable();
+		var contacts = ko.observableArray([
+			{ name: "Contact 1", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" },
+			{ name: "Contact 2", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" },
+			{ name: "Contact 3", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "friend" },
+			{ name: "Contact 4", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "colleague" },
+			{ name: "Contact 5", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" },
+			{ name: "Contact 6", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "colleague" },
+			{ name: "Contact 7", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "friend" },
+			{ name: "Contact 8", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" }
+		]);
 
-	self.hide_contact = function(e){
-		if(e.nodeType === 1){
-			$(e).fadeOut();
-		}
-	};
+		self.selected_type = ko.observable();
 
-	self.show_contact = function(e){
-		if(e.nodeType === 1){
-			$(e).fadeIn();
-		}
-	};
+		self.hide_contact = function(e){
+			if(e.nodeType === 1){
+				$(e).fadeOut();
+			}
+		};
 
-	self.add_contact = function(form) {
-		var new_contact = {},
-			fields = $(form).serializeArray();
+		self.show_contact = function(e){
+			if(e.nodeType === 1){
+				$(e).fadeIn();
+			}
+		};
 
-		for(var i=0,j;j = fields[i++]; ){
-			new_contact[j.name] = j.value;
-		}
+		self.add_contact = function(form) {
+			var new_contact = {},
+				fields = $(form).serializeArray();
 
-		contacts.push(new_contact);
-		
-		$(form)[0].reset();
-		return false;
-	};
+			for(var i=0,j;j = fields[i++]; ){
+				new_contact[j.name] = j.value;
+			}
 
-	self.filtered_contacts = ko.computed(function(){
-		return $.map(contacts(), function(v){ 
-			if(typeof self.selected_type() == 'undefined' || v.type == self.selected_type()) 
-				return v; 
+			contacts.push(new_contact);
+			
+			$(form)[0].reset();
+			return false;
+		};
+
+		self.filtered_contacts = ko.computed(function(){
+			return $.map(contacts(), function(v){ 
+				if(typeof self.selected_type() == 'undefined' || v.type == self.selected_type()) 
+					return v; 
+			});
 		});
-	});
 
-	self.photo = 'placeholder.png',
-	self.types = ko.computed(function(){
-		return $.unique($.map(contacts(), function(v){ return v.type; }));
-	});
-};
+		self.photo = 'placeholder.png',
+		self.types = ko.computed(function(){
+			return $.unique($.map(contacts(), function(v){ return v.type; }));
+		});
+	};
 
-ko.applyBindings(new ContactsView());
+	this.init = function(){
+		ko.applyBindings(new ContactsView());
+	}
+}	
+
